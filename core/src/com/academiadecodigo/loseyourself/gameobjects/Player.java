7478e81777ae3edfb97420098972b5f37740be9f
Ana Cergol <nanacergol@hotmail.com>
2017-11-23 20:36:15 +0000

    resolving conflicts and remove movable interface

--- core/src/com/academiadecodigo/loseyourself/gameobjects/Player.java
+++ core/src/com/academiadecodigo/loseyourself/gameobjects/Player.java
@@ -1,24 +1,14 @@
 package com.academiadecodigo.loseyourself.gameobjects;
 
-<<<<<<< HEAD
-public class Player implements Movable {
-
-    @Override
-    public void move() {
-
-    }
-
-
-
-
-=======
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 
+
 public class Player {
+
 
     private SpriteBatch batch;
     private Texture img;
@@ -44,10 +34,11 @@ public class Player {
     }
 
     private void teste() {
-        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) this.rectangle.x -= 200 * Gdx.graphics.getDeltaTime();
-        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) this.rectangle.x += 200 * Gdx.graphics.getDeltaTime();
-        if(Gdx.input.isKeyPressed(Input.Keys.UP)) this.rectangle.y += 200 * Gdx.graphics.getDeltaTime();
-        if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) this.rectangle.y -= 200 * Gdx.graphics.getDeltaTime();
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) this.rectangle.x -= 200 * Gdx.graphics.getDeltaTime();
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) this.rectangle.x += 200 * Gdx.graphics.getDeltaTime();
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) this.rectangle.y += 200 * Gdx.graphics.getDeltaTime();
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) this.rectangle.y -= 200 * Gdx.graphics.getDeltaTime();
     }
->>>>>>> b07ddf4b4c1724e2ec8824378a3a1d831e4746d5
+
 }
+

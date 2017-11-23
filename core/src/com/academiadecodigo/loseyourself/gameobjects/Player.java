51ce8445c427317e683b5797fe9c9c6ce38960ba
BrunoM24 <brunocostamatos.24@gmail.com>
2017-11-23 21:07:37 +0000

    Player movement

--- core/src/com/academiadecodigo/loseyourself/gameobjects/Player.java
+++ core/src/com/academiadecodigo/loseyourself/gameobjects/Player.java
@@ -3,38 +3,73 @@ package com.academiadecodigo.loseyourself.gameobjects;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 
 public class Player {
 
     private SpriteBatch batch;
-    private Texture img;
     private Rectangle rectangle;
+    private int dir = 0;
+    private Sprite sprite;
 
     public Player() {
         batch = new SpriteBatch();
-        img = new Texture("eminem2.png");
+        this.sprite = new Sprite(new Texture("eminem.png"));
         this.rectangle = new Rectangle();
         this.rectangle.x = 0;
         this.rectangle.y = 0;
-        this.rectangle.width = this.img.getWidth();
-        this.rectangle.height = this.img.getHeight();
+        this.rectangle.width = this.sprite.getWidth();
+        this.rectangle.height = this.sprite.getHeight();
     }
 
     public void render() {
-
-        teste();
+        inputHandler();
 
         this.batch.begin();
-        this.batch.draw(img, this.rectangle.x, this.rectangle.y);
+        sprite.draw(batch);
         this.batch.end();
+
+        System.out.println(dir);
     }
 
-    private void teste() {
-        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) this.rectangle.x -= 200 * Gdx.graphics.getDeltaTime();
-        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) this.rectangle.x += 200 * Gdx.graphics.getDeltaTime();
-        if(Gdx.input.isKeyPressed(Input.Keys.UP)) this.rectangle.y += 200 * Gdx.graphics.getDeltaTime();
-        if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) this.rectangle.y -= 200 * Gdx.graphics.getDeltaTime();
+    private void inputHandler() {
+        if(Gdx.input.isKeyPressed(Input.Keys.UP)){
+            move();
+            return;
+        }
+
+        if(Gdx.input.isKeyJustPressed(Input.Keys.LEFT)){
+            this.sprite.rotate(90);
+            if(this.dir == 0){
+                this.dir = 3;
+            }else {
+                this.dir--;
+            }
+
+        }else if(Gdx.input.isKeyJustPressed(Input.Keys.RIGHT)){
+            this.sprite.rotate(-90);
+            if(this.dir == 3){
+                this.dir = 0;
+            }else {
+                this.dir++;
+            }
+        }
+    }
+
+    private void move() {
+        if(dir == 0){
+            this.sprite.setY(this.sprite.getY() + 200 * Gdx.graphics.getDeltaTime());
+        }
+        if(dir == 1){
+            this.sprite.setX(this.sprite.getX() + 200 * Gdx.graphics.getDeltaTime());
+        }
+        if(dir == 2){
+            this.sprite.setY(this.sprite.getY() - 200 * Gdx.graphics.getDeltaTime());
+        }
+        if(dir == 3){
+            this.sprite.setX(this.sprite.getX() - 200 * Gdx.graphics.getDeltaTime());
+        }
     }
 }
